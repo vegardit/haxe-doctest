a1aa4654559830332836b6ad7a85b9d7178891a1
Sebastian Thomschke <sebthom@users.noreply.github.com>
2017-08-07 22:24:14 +0200

    workaround for cpp in test case

--- test/hx/doctest/tests/DocTestTest.hx
+++ test/hx/doctest/tests/DocTestTest.hx
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016-2017 Vegard IT GmbH, http://vegardit.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,7 @@ import hx.doctest.DocTestRunner;
 
 /**
  * Performs doc-testing with DocTestRunner.
- * 
+ *
  * @author Sebastian Thomschke, Vegard IT GmbH
  */
 @:build(hx.doctest.DocTestGenerator.generateDocTests("test"))
@@ -29,11 +29,11 @@ class DocTestTest extends DocTestRunner {
         var runner = new DocTestTest();
         runner.runAndExit(20 /* number of expected test cases */);
     }
-    
+
     function new() {
         super();
     }
-    
+
     /**
      * Manually added test method to do some additional non-doctest based testing
      */
@@ -45,10 +45,10 @@ class DocTestTest extends DocTestRunner {
              * assigning null not on first assignment to make code work in Lua, otherwise
              * the block will be transpiled into `nil:toLowerCase()` which doesn't compile in Lua
              */
-            var s = ""; 
+            var s = "";
             s = null;
-            s.toLowerCase(); // throws NPE ... except on PHP
-            #if !php
+            s.toLowerCase(); // throws NPE ... except on PHP and C++
+            #if (!php && !cpp)
             fail(); // should never be reached
             #end
         } catch (e:Dynamic) {
