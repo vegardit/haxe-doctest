1ec19fc53aa20578c6cf3a0293637b5c66c7204e
Sebastian Thomschke <sebthom@users.noreply.github.com>
2019-11-14 23:17:26 +0100

    add jvm target test

---
+++ tools/test-jvm.cmd
@@ -0,0 +1,15 @@
+@echo off
+REM Copyright (c) 2016-2019 Vegard IT GmbH, https://vegardit.com
+REM SPDX-License-Identifier: Apache-2.0
+REM Author: Sebastian Thomschke, Vegard IT GmbH
+
+call %~dp0_test-prepare.cmd java hxjava
+
+echo Compiling...
+haxe %~dp0..\tests.hxml -D jvm -java target\java
+set rc=%errorlevel%
+popd
+if not %rc% == 0 exit /b %rc%
+
+echo Testing...
+java -jar "%~dp0..\target\java\TestRunner-Debug.jar"
