1f71bcd5e56d0a89268d3f136153df77a728a727
sebthom <sebthom@users.noreply.github.com>
2021-05-06 19:30:08 +0200

    exclude using testrunner with PHP

--- test/hx/doctest/TestRunner.hx
+++ test/hx/doctest/TestRunner.hx
@@ -34,7 +34,7 @@ class TestRunner {
        * 2) fails on Flash with: lib\mconsole/1,6,0/mconsole/Console.hx:469: characters 3-94 : flash.utils.Object cannot be called
        *
        * 3) fails on JS with:
-       *   ReferenceError: Can't find variable: addToQueue
+       *    ReferenceError: Can't find variable: addToQueue
        *     undefined:1 in eval code
        *     :0 in eval
        *     phantomjs://code/TestRunner.js:2902 in queue
@@ -52,13 +52,20 @@ class TestRunner {
       MUnitTest.main();
       #end
 
-      #if !(flash || nodejs)
+      #if !(flash || nodejs || php)
       /*
        * 1) fails on nodejs with:
        *    tink_testrunner/0,7,2/src/tink/testrunner/Reporter.hx:180: characters 4-15 : Accessing this field requires a system platform (php,neko,cpp,etc.)
        *
        * 2) fails on Flash with:
        *    Not supported yet.
+       *
+       * 3) fails on PHP with:
+       *    Fatal error: Uncaught Error: Call to undefined method Attribute::Off() in haxe-doctest/bin/php/lib/ANSI.php:110
+       *    Stack trace:
+       *    #0 haxe-doctest/bin/php/lib/ANSI.php(145): ANSI::__hx__init()
+       *    #1 haxe-doctest/bin/php/index.php(10): include_once('/Users/runner/w...')
+       *    #2 haxe-doctest/bin/php/lib/tink/testrunner/AnsiFormatter.php(45): {closure}('ANSI')
        */
       trace("###################");
       trace("TinkTestrunnerUnitTest");
